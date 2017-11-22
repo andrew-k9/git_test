ac48fea7bfae3d28ac94cff21a6aa8e2bd9891a9
andrew <lt.kvothe@gmail.com>
2017-11-21 23:45:52 -0500

    Indentation changes and added check for correct input

--- insert.rb
+++ insert.rb
@@ -1,14 +1,23 @@
+# a staandard implemintation of insertion sort
 def insert(arr)
+
+  if !arr.is_a?(Array)
+    puts "An error has occured. arr is of type #{arr.class}"
+    return -1
+  end
+
   (1...arr.length).each do |i|
     if arr[i] <= arr[i - 1]
     	j = i
     	while j > 0 and arr[j] < arr[j - 1]
     		temp = arr[j]
     		arr[j] = arr[j - 1]
-			arr[j - 1] = temp
-			j -= 1
-		end 
-	end
+			  arr[j - 1] = temp
+			  j -= 1
+		  end 
+	  end
   end
-	return arr
+
+  arr
+
 end
