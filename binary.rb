0be8c652dd2380e0720d090988ef1b3036e12d33
andrew <lt.kvothe@gmail.com>
2017-11-22 00:05:00 -0500

    Indentation fix, check for correct input, and change of method name

--- binary.rb
+++ binary.rb
@@ -1,13 +1,26 @@
-
-def delta(x,y)
+# returns the middle of the to indicies
+def middle(x,y)
   return ((x + y) / 2 ).floor
 end
 
+# Standard implemintation of binary search algorithm.
+# Expects sorted array, and returns -1 if the index is not present
 def binary(arr, n)
   
+  if !arr.is_a?(Array) || !n.is_a?(Fixnum)
+    if !arr.is_a?(Array) && n.is_a?(Fixnum)
+      puts "An error has occured. arr is of type #{arr.class}"
+    elsif !n.is_a?(Fixnum) && arr.is_a?(Array)
+      puts "An error has occured. n is of type #{n.class}"
+    else
+      puts "An error has occured. arr is of type #{arr.class} and n is of type #{n.class} "
+    end
+    return -1
+  end
+
   left  = 0
   right = arr.length - 1
-  mid   = delta(left,right)
+  mid   = middle(left,right)
   ans   = -1
 
   while left <= right
@@ -15,11 +28,11 @@ def binary(arr, n)
       ans = mid
       left = right + 1
     elsif n < arr[mid]
-	   right = mid - 1
-	   mid = delta(left,right)
+	    right = mid - 1
+	    mid = middle(left,right)
     else
-     left = mid  + 1
-     mid  = delta(left,right)
+      left = mid  + 1
+      mid  = middle(left,right)
     end
   end
 
